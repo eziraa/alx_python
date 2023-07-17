4a3f6d1a156eb72855832a1905ed2aafa2c93ace
dessallegn <desutilaye2015@gmail.com>
2023-07-17 13:40:43 +0300

    The project two question were done

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:3]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
