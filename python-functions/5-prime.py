e8ffa7d39a8460965ff3588bfff30e548d0eee6f
sutual <ezirayallew@gmail.com>
2023-07-20 16:46:50 +0300

    The projecct is dome

---
+++ python-functions/5-prime.py
@@ -0,0 +1,13 @@
+#!/bin/bash/python3
+def is_prime(number):
+    i = 2
+    count = 0
+    while (i < number):
+        if (number % i == 0):
+            count += 1
+            i += 1
+
+    if (count == 0):
+        return "True"
+    else:
+        return "False"
