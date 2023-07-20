cd18d70046e64267c793955d198bc7a9b40b6993
sutual <ezirayallew@gmail.com>
2023-07-20 15:55:32 +0300

    The problem is solved

---
+++ python-functions/4-fibonanci.py
@@ -0,0 +1,17 @@
+#!/bin/bash/python3
+def fibonacci_sequence(n):
+    list = []
+    if (n == 0):
+        list.append(0)
+    elif (n == 1):
+        list.append(0)
+        list.append(1)
+    else:
+        i = 2
+        list.append(0)
+        list.append(1)
+        while (i < n):
+            list.append(list[i-1] + list[i - 2])
+            i = i + 1
+
+    return list
