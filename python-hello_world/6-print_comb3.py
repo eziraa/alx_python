1a5f12fe0cf66b37acb3717b9da2a06de4414eb8
sutual <ezirayallew@gmail.com>
2023-07-20 14:28:15 +0300

    THe project is done

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d}".format(i), end=', ')
-print("{:02d}".format(i + 1))
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
