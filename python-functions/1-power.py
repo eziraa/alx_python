193e1fa7f058b256c8151d414ee54c0571c462b7
sutual <ezirayallew@gmail.com>
2023-07-20 14:48:45 +0300

    The project is done

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,5 +1,9 @@
 #!/bin/bash/python3
 def pow(a, b):
-    for i in range(b):
-        a = a * a
+    if (b > 0):
+        for i in range(b):
+            a = a * a
+    else:
+        for i in range(b):
+            a = a / a
     return a
