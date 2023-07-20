421a0f077803fc4e52aa73381dac7735bf67cab2
sutual <ezirayallew@gmail.com>
2023-07-20 14:13:40 +0300

    THe last digit project is done

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = 0
+if (number >= 0):
+    last = number % 10
+else:
+    last = ((-1 * number) % 10) * -1
+msg = ""
+if (last == 0):
+    msg = "is zero"
+elif (last > 5):
+    msg = "is greater than 5"
+else:
+    msg = "is less than 6 and not 0"
+print(f"Last digit of {number} is {last} and {msg}")
