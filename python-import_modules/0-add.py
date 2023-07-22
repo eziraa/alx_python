14a51c1886cf0f37dac56ed26c4798c4402eed35
sutual <ezirayallew@gmail.com>
2023-07-22 18:34:36 +0300

    The problem is solved

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!?bin/bash/puthon3
 if __name__ == "__main__":
     """Print some of 1 an 2"""
-    from add_0.py import add
+    from add_0 import add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
