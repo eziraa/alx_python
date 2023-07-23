f58b3904dac43ce24e74e195e2494d993bf4f9db
sutual <ezirayallew@gmail.com>
2023-07-23 16:12:37 +0300

    The project is done

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,25 @@
+#!/bin/bash/python3
+import sys
+
+
+def print_arguments():
+    # Exclude the first element (script name) from the list
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
+
+    # Print the number of arguments and the list of arguments
+    print(f"Number of argument(s): {num_arguments}", end=" ")
+    print("argument" if num_arguments == 1 else "arguments", end="")
+
+    if num_arguments == 0:
+        print(".", end="\n")
+    else:
+        print(":", end="\n")
+
+        # Print one line per argument along with its position and value
+        for i, arg in enumerate(arguments, start=1):
+            print(f"{i}: {arg}")
+
+
+if __name__ == "__main__":
+    print_arguments()
