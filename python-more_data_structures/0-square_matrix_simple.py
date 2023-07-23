2a9ee679c21380960c8f0e9cd9328fe305881fa0
sutual <ezirayallew@gmail.com>
2023-07-24 00:36:50 +0300

    The problme is solved

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -9,6 +9,3 @@ def square_matrix_simple(matrix=[]):
         new_matrix.append(list)
     new_matrix.remove([]) if [] not in matrix else new_matrix
     return new_matrix
-
-
-print(square_matrix_simple([[4, 1, 4], [4, 52, 5]]))
