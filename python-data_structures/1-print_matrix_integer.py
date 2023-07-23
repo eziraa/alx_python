91338fa3a02184acd2c162f004bb232bc4bf4682
sutual <ezirayallew@gmail.com>
2023-07-23 23:46:36 +0300

    The problme si solved

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # Iterate through each row in the matrix
     for row in matrix:
-        # Iterate through each element in the row and print it using str.format()
         for e in row:
             if e != row[-1]:
                 print("{:d}".format(e), end=' ')
             else:
                 print("{:d}".format(e), end='')
-        # Move to the next line after printing each row
         print()
