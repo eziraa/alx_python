ae6beb3904ba28962433f48dc618708efab59075
sutual <ezirayallew@gmail.com>
2023-07-24 01:12:35 +0300

    The ptrojct is done

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,8 +1,5 @@
 def update_dictionary(a_dictionary, key, value):
-    is_updated = False
-    for key1, value in a_dictionary:
-        if (key == key1):
-            dict(a_dictionary).update(key, value)
-            is_updated = True
-    if (not is_updated):
-        dict(a_dictionary(key, value))
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
