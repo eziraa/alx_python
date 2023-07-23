76073414764c9ae5fe6a68a6ced940cd1ffc58e5
sutual <ezirayallew@gmail.com>
2023-07-23 22:36:34 +0300

    THe project is done

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,14 @@
+#!/bin/bash/python3
+def no_c(string):
+    list = ""
+
+    for i in string:
+        if i == 'c' or i == 'C':
+            continue
+        else:
+            list += i
+
+    return list
+
+
+print(str(no_c('cdf')))
