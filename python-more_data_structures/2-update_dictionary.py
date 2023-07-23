b871ca607fd5e9df4856cb871e9a65ef4ae28c70
sutual <ezirayallew@gmail.com>
2023-07-24 01:15:14 +0300

    The project is done

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,5 +1,9 @@
+#!/usr/bin/puthon3
 def update_dictionary(a_dictionary, key, value):
+    # Check if the key already exists in the dictionary
     if key in a_dictionary:
+        # If the key exists, update the value
         a_dictionary[key] = value
     else:
+        # If the key doesn't exist, add a new key-value pair
         a_dictionary[key] = value
