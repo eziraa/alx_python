88f005fe8253ab903daf09fdc0b3a55a4bc257fe
sutual <ezirayallew@gmail.com>
2023-07-23 23:33:00 +0300

    The project is done

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,6 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         # Iterate through each element in the row and print it using str.format()
         for element in row:
-            print("{:d}".format(element), end='')
+            print("{:d}".format(element), end=' ' if element !=
+                  row[len(row) - 1] else '')
         # Move to the next line after printing each row
         print()
