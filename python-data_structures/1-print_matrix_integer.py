a63c33077434e75242585ce853a021b6aefdf9b5
sutual <ezirayallew@gmail.com>
2023-07-23 23:23:35 +0300

    The project is done

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,27 +1,9 @@
 #!/bin/bash/python3
 def print_matrix_integer(matrix=[[]]):
-    # Get the number of rows and columns in the matrix
-    num_rows = len(matrix)
-    num_cols = len(matrix[0]) if num_rows > 0 else 0
-
-    # Calculate the maximum width of each column to align the output
-    column_widths = [max(len(str(matrix[row][col]))
-                         for row in range(num_rows)) for col in range(num_cols)]
-
-    # Print the matrix with proper formatting
+    # Iterate through each row in the matrix
     for row in matrix:
-        for col_idx, value in enumerate(row):
-            # Use str.format() to print each value with appropriate width
-            print("{:>{width}}".format(
-                value, width=column_widths[col_idx]), end=" ")
-        print()  # Move to the next row after printing all values
-
-
-# Test the function
-matrix = [
-    [1, 22, 333],
-    [4444, 55555, 666666],
-    [7777777, 88888888, 999999999]
-]
-
-print_matrix_integer(matrix)
+        # Iterate through each element in the row and print it using str.format()
+        for element in row:
+            print("{:d}".format(element), end=' ')
+        # Move to the next line after printing each row
+        print()
