12ded82f13532dd78e27d9e7b7502dc2171bc2e5
sutual <ezirayallew@gmail.com>
2023-07-23 23:37:37 +0300

    The problme si solved

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,8 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     # Iterate through each row in the matrix
     for row in matrix:
         # Iterate through each element in the row and print it using str.format()
-        for elem in row:
-            print("{:d}".format(elem), end=' ' if elem !=
-                  row[len(row) - 1] else '')
+        for e in row:
+            print("{:d}".format(e), end=' ' if e != row[len(row) - 1] else '')
         # Move to the next line after printing each row
         print()
