0e649246f1943a98ae5e74e4f880262107e0bab3
sutual <ezirayallew@gmail.com>
2023-07-25 21:51:36 +0300

    The problrm is solved

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -3,10 +3,8 @@ def safe_print_division(a, b):
     try:
         return a / b
     except ZeroDivisionError:
-        print('The divisor should be non zero')
         return None
     except TypeError:
-        print('The number should both integer')
         return None
     finally:
         print("Inside result: {}".format(a / b if b != 0 else None))
