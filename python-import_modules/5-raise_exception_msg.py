b36e86fbfd0a9f08c468dbaa03fddf5da544e4bc
sutual <ezirayallew@gmail.com>
2023-07-25 22:05:45 +0300

    The problem is solved

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -2,14 +2,4 @@
 def raise_exception_msg(message=""):
     class CustomNameException(NameError):
         pass
-    # if not message:
-    try:
-        raise CustomNameException(message)
-    except CustomNameException:
-        pass
-
-
-try:
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
+    raise CustomNameException(message)
